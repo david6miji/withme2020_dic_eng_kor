1d564ba09aad2f8e141abb90655765c83331e0a1
david6miji <frog@falinux.com>
2016-06-16 19:31:09 +0900

    backup

--- util/cvt_stardict_to_withme_dic_step1.js
+++ util/cvt_stardict_to_withme_dic_step1.js
@@ -56,7 +56,7 @@ line_reader.on('line', function (line) {
 	word_lines.forEach( function(word_line,i) {
 		
 		if( word_line === "" ) { return; }
-//		console.log('word_line[' + i + ']' + ' = '+ word_line );
+		console.log('word_line[' + i + ']' + ' = '+ word_line );
 		
 		// 2 번째 처리 과정 
 		var word_line = word_line.split( word_split_mark );
@@ -90,28 +90,140 @@ line_reader.on('line', function (line) {
 //					console.log('       ' + '[' +  item + ']' );
 					switch(item){
 					case 'a'    : word.parts.push('a'  ); 		break; // 형용사
+					case 'A'    : word.parts.push('a'  ); 		break; // 형용사
 					case 'ad'   : word.parts.push('ad' );    	break; // 부사
 					case 'aux'  : word.parts.push('aux' );    	break; // 조동사
+					case 'auxil': word.parts.push('aux' );    	break; // 조동사
 					case 'conj' : word.parts.push('conj' );    break; // 접속사
 					case 'int'  : word.parts.push('int');      break; // 감탄사
 					case 'n'    : word.parts.push('n'  );     	break; // 명사
+					case 'N'    : word.parts.push('n'  );     	break; // 명사
+					case '(n)'  : word.parts.push('n'  );     	break; // 명사
 					case 'pl'   : word.parts.push('pl' );     	break; // 복수 명사
 					case 'prep' : word.parts.push('prep' );   	break; // 전치사
 					case 'pron' : word.parts.push('pron' );   	break; // 대명사
 					case 'vi'   : word.parts.push('vi' );  	break; // 자동사
+					case '-col()vi'   : word.parts.push('vi' );  	break; // 자동사
 					case 'vt'   : word.parts.push('vt' );    	break; // 타동사
+					case 'VT'   : word.parts.push('vt' );    	break; // 타동사
+					case 'v t'  : word.parts.push('vt' );    	break; // 타동사
 					case 'v'    : word.parts.push('v'  );    	break; // 동사
+					case 'V'    : word.parts.push('v'  );    	break; // 동사
 					case 'x'    : word.parts.push('x'  );    	break; // 복합명사
+					case 'X'    : word.parts.push('x'  );    	break; // 복합명사
 					case 'rel'  : word.parts.push('rel' );     break; // 관계사
 					case 'suf'  : word.parts.push('suf' );   	break; // 접미사
 					case 'pref' : word.parts.push('pref' );   	break; // 접두사
+					case 'indef': word.parts.push('pi' );   	break; // 부정(不定) 대명사
+					case 'sing'	: word.parts.push('pi' );   	break; // 단수형
 
-					case 'n.pl' : word.parts.push('n.pl' );    break; //
+					case 'vn'   : word.parts.push('n' );   
+								  word.parts.push('v' );   	 
+								  break; //
+					case 'va'   : word.parts.push('v' );   
+								  word.parts.push('a' );   	 
+								  break; //
+					case 'n.a'   : word.parts.push('n' );   
+								  word.parts.push('a' );   	 
+								  break; //
+					case 'na'   : word.parts.push('n' );   
+								  word.parts.push('a' );   	 
+								  break; //
+					case 'a.n'   : word.parts.push('n' );   
+								  word.parts.push('a' );   	 
+								  break; //
+					case 'a n'   : word.parts.push('n' );   
+								  word.parts.push('a' );   	 
+								  break; //
+								  
+					case 'n.pl'   : word.parts.push('n' );   
+								  word.parts.push('pl' );   	 
+								  break; //
+					case 'vt.vi'   : word.parts.push('vt' );   
+								  word.parts.push('vi' );   	 
+								  break; //
+					case 'n\vt'   : word.parts.push('n' );   
+								  word.parts.push('vt' );   	 
+								  break; //
+					case 'x\a'   : word.parts.push('x' );   
+								  word.parts.push('a' );   	 
+								  break; //
+					case 'n.vt'   : word.parts.push('n' );   
+								  word.parts.push('vt' );   	 
+								  break; //
+					case 'n.ad'   : word.parts.push('n' );   
+								  word.parts.push('ad' );   	 
+								  break; //
+					case 'x.vt'   : word.parts.push('x' );   
+								  word.parts.push('vt' );   	 
+								  break; //
+					case 'xn'   : word.parts.push('x' );   
+								  word.parts.push('n' );   	 
+								  break; //
+					case 'vtn'   : word.parts.push('vt' );   
+								  word.parts.push('n' );   	 
+								  break; //
+					case 'a.ad'   : word.parts.push('a' );   
+								  word.parts.push('ad' );   	 
+								  break; //
+					case 'a.vt'   : word.parts.push('a' );   
+								  word.parts.push('vt' );   	 
+								  break; //
+					case 'a.vi'   : word.parts.push('a' );   
+								  word.parts.push('vi' );   	 
+								  break; //
+					case 'nad'   : word.parts.push('n' );   
+								  word.parts.push('ad' );   	 
+								  break; //
+								  
+					case 'v.vi'  : word.parts.push('vi'  ); 		break; // 동사
+					case '...vi' : word.parts.push('vi'  ); 		break; // 동사
+					case '...vt' : word.parts.push('vt'  ); 		break; // 동사
+					
+					case ':a'    : word.parts.push('a'  ); 		break; // 형용사
+					case 'a)'    : word.parts.push('a'  ); 		break; // 형용사
+					case '...a'  : word.parts.push('a'  ); 		break; // 형용사
+					case '...n'  : word.parts.push('n'  ); 		break; // 형용사
+					
+					case ':n'    : word.parts.push('n'  ); 		break; // 명사
+					case 'v i'    : word.parts.push('vi'  ); 		break; // 동사
+					case 'a d'    : word.parts.push('ad'  ); 		break; // 동사
+								  
 					case 'pred' : word.parts.push('pred' );   	break; // 
 					case 'pi'   : word.parts.push('pi' );   	break; // 
+					case 'pt'   : word.parts.push('pt' );   	break; // 
+					case 'ti'   : word.parts.push('ti' );   	break; // 
+					case 'xint' : word.parts.push('xint' );   	break; // 
+					case 'xprep': word.parts.push('xprep' ); 	break; // 
+					case 'xconj': word.parts.push('xconj' ); 	break; // 
+					case 'xbent': word.parts.push('xbent' ); 	break; // 
+					case 'xa'	: word.parts.push('xa' ); 		break; // 
+					case 's'	: word.parts.push('s' ); 		break; // 
+					case 'd'	: word.parts.push('d' ); 		break; // 
+					case 'z'	: word.parts.push('z' ); 		break; // 
+					case 'in'	: word.parts.push('in' ); 		break; // 
+					case 'dn'	: word.parts.push('dn' ); 		break; // 
+					case 'an'	: word.parts.push('an' ); 		break; // 
+					case 'f'	: word.parts.push('f' ); 		break; // 
+					case 'q'	: word.parts.push('q' ); 		break; // 
+					case 'b'	: word.parts.push('b' ); 		break; // 
+					case 'm'	: word.parts.push('m' ); 		break; // 
+					case 'c'	: word.parts.push('c' ); 		break; // 
+					case 'adv'	: word.parts.push('adv' ); 		break; // 
+					case 'as'	: word.parts.push('as' ); 		break; // 
+					case 'ints'	: word.parts.push('ints' ); 		break; // 
+					case 'avt'	: word.parts.push('avt' ); 		break; // 
+					case 'av'	: word.parts.push('av' ); 		break; // 
 					
 					
-					case ''     : break;			  
+					case 'cost': 
+									console.log('Unknown parts ' + '[' +  item + ']' ); 
+									console.log('       word_line[0] = ' +  word.eng );  
+									console.log('       word_line[1] = [' +  word_line[1] + ']' );
+								   break;
+					
+					case ''     : break;
+					
 							   
 					default  : if( item[0] === '=' ){
 									phase_checking_part = false;
